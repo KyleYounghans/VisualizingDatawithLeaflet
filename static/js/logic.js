492c218a0d0c7b456965c124aa2b0f5e651400c8
Kyle Younghans <kylej4880@gmail.com>
2019-10-12 21:00:12 -0500

    Adding Code
    Adding Code

---
+++ static/js/logic.js
@@ -0,0 +1,129 @@
+var API_quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+console.log (API_quakes)
+var API_plates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+console.log (API_plates)
+
+function markerSize(magnitude) {
+    return magnitude * 4;
+};
+
+
+var earthquakes = new L.LayerGroup();
+
+d3.json(API_quakes, function (geoJson) {
+    L.geoJSON(geoJson.features, {
+        pointToLayer: function (geoJsonPoint, latlng) {
+            return L.circleMarker(latlng, { radius: markerSize(geoJsonPoint.properties.mag) });
+        },
+
+        style: function (geoJsonFeature) {
+            return {
+                fillColor: Color(geoJsonFeature.properties.mag),
+                fillOpacity: 0.7,
+                weight: 0.1,
+                color: 'black'
+
+            }
+        },
+
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                "<h4 style='text-align:center;'>" + new Date(feature.properties.time) +
+                "</h4> <hr> <h5 style='text-align:center;'>" + feature.properties.title + "</h5>");
+        }
+    }).addTo(earthquakes);
+    createMap(earthquakes);
+});
+
+var faultLines = new L.LayerGroup();
+
+d3.json(API_plates, function (geoJson) {
+    L.geoJSON(geoJson.features, {
+        style: function (geoJsonFeature) {
+            return {
+                weight: 2,
+                color: '#FEAF25'
+            }
+        },
+    }).addTo(faultLines);
+})
+
+
+function Color(magnitude) {
+    if (magnitude > 5) {
+        return '#800'
+    } else if (magnitude > 4) {
+        return '#f00'
+    } else if (magnitude > 3) {
+        return '#ff9100'
+    } else if (magnitude > 2) {
+        return '#ff0'
+    } else if (magnitude > 1) {
+        return '#3EAE16'
+    } else {
+        return '#76FEEC'
+    }
+};
+
+function createMap() {
+
+    var satelliteMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.satellite',
+        accessToken: API_KEY
+    });
+
+    var lightMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.light',
+        accessToken: API_KEY
+    });
+
+    var outdoorsMap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.outdoors',
+        accessToken: API_KEY
+    });
+
+    var baseLayers = {
+        "Satellite": satelliteMap,
+        "Grayscale": lightMap,
+        "Outdoors": outdoorsMap,
+    };
+
+    var overlays = {
+        "Fault Lines": faultLines,
+        "Earthquakes": earthquakes,
+    };
+
+    var mymap = L.map('mymap', {
+        center: [40, -99],
+        zoom: 4.3,
+        layers: [satelliteMap, earthquakes, faultLines]
+    });
+
+    L.control.layers(baseLayers, overlays).addTo(mymap);
+
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function (map) {
+
+        var div = L.DomUtil.create('div', 'info legend'),
+            magnitude = [0, 1, 2, 3, 4, 5],
+            labels = [];
+
+        div.innerHTML += "<h4 style='margin:4px'>Magnitude</h4>"
+
+        for (var i = 0; i < magnitude.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + Color(magnitude[i] + 1) + '"></i> ' +
+                magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+    legend.addTo(mymap);
+}
